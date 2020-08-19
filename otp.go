a1fefa740962b96bffc55545e3a74f1cb5cd9918
Jose Torres <jose.torres.tm@gmail.com>
2020-08-19 18:46:54 -0500

    Add the actual logic to calculate the tokens.

---
+++ otp.go
@@ -0,0 +1,51 @@
+package otpgo
+
+import (
+	"crypto/hmac"
+	"crypto/rand"
+	"encoding/base32"
+	"encoding/binary"
+)
+
+const RandomKeyLength = 64
+
+// Generates a new OTP using the specified parameters based on the rfc4226.
+func generateOTP(key string, counter uint64, length otpLength, algorithm hmacAlgorithm) (string, error) {
+	// Decode secret key to bytes
+	k, err := base32.StdEncoding.DecodeString(key)
+	if err != nil {
+		return "", ErrorInvalidKey{msg: err.Error()}
+	}
+
+	// Convert the counter to bytes
+	msg := make([]byte, 8)
+	binary.BigEndian.PutUint64(msg, counter)
+
+	// Start the hmac algorithm
+	hm := hmac.New(algorithm.Hash, k)
+	hm.Write(msg)
+	sum := hm.Sum([]byte{})
+
+	// Build the result integer
+	offset := sum[len(sum)-1] & 0xf
+
+	bin := ((int(sum[offset]) & 0x7f) << 24) |
+		((int(sum[offset+1]) & 0xff) << 16) |
+		((int(sum[offset+2]) & 0xff) << 8) |
+		(int(sum[offset+3]) & 0xff)
+
+	rawOtp := length.Truncate(bin)
+	otp := length.LeftPad(rawOtp)
+
+	return otp, nil
+}
+
+// Generates a random key of the specified length, usable for OTP generation.
+func randomKey(length int) (string, error) {
+	buff := make([]byte, length)
+	if _, err := rand.Read(buff); err != nil {
+		return "", err
+	}
+
+	return base32.StdEncoding.EncodeToString(buff), nil
+}
