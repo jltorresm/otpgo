ca9d506913a43c11842691404d110c10b73d30a7
Jose Torres <jose.torres.tm@gmail.com>
2020-08-25 11:22:42 -0500

    Add validation for time based tokens.

--- totp.go
+++ totp.go
@@ -1,6 +1,7 @@
 package otpgo
 
 import (
+	"errors"
 	"math"
 	"time"
 )
@@ -8,12 +9,17 @@ import (
 const (
 	// TOTPDefaultPeriod is the default time period to use if none is provided by the caller.
 	TOTPDefaultPeriod = 30
+	// TOTPDefaultDelay is the default acceptable delay window. A value of 1
+	// means the OTP will be valid if it coincides with the calculated token for
+	// the current time step, the next one or the one before.
+	TOTPDefaultDelay = 1
 )
 
 // The TOTP type used to generate Time-Based One-Time Passwords.
 type TOTP struct {
 	Key       string // Secret
 	Period    int    // In seconds
+	Delay     int    // Acceptable steps for network delay
 	Algorithm hmacAlgorithm
 	Length    otpLength
 }
@@ -29,10 +35,48 @@ func (t *TOTP) Generate() (string, error) {
 	}
 
 	// Get the counter based on the current time
-	timestamp := time.Now().Unix()
-	counter := uint64(math.Floor(float64(timestamp) / float64(t.Period)))
+	counter := t.getCounter(time.Now().Unix())
 
 	return generateOTP(t.Key, counter, t.Length, t.Algorithm)
+}
+
+// Validate will try to check if the provided token is a valid OTP for the current TOTP config.
+func (t *TOTP) Validate(token string) (bool, error) {
+	// This will be the base for all validations
+	now := time.Now().Unix()
+
+	// Validating without a proper key shouldn't happen
+	if t.Key == "" {
+		return false, errors.New("missing secret key for validation")
+	}
+
+	// Make sure we have sensible values to generate secure OTPs
+	t.ensureDefaults()
+
+	// Now go through all the possible valid tokens
+	for step := 0; step <= t.Delay; step++ {
+		pad := int64(t.Period * step)
+		under := t.getCounter(now - pad)
+
+		expected, err := generateOTP(t.Key, under, t.Length, t.Algorithm)
+		if err != nil {
+			return false, err
+		}
+		if expected == token {
+			return true, nil
+		}
+
+		over := t.getCounter(now + pad)
+		expected, err = generateOTP(t.Key, over, t.Length, t.Algorithm)
+		if err != nil {
+			return false, err
+		}
+		if expected == token {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
 
 // ensureDefaults applies sensible default values, if any of them is empty, so
@@ -44,6 +88,10 @@ func (t *TOTP) Generate() (string, error) {
 func (t *TOTP) ensureDefaults() {
 	if t.Period == 0 {
 		t.Period = TOTPDefaultPeriod
+	}
+
+	if t.Delay == 0 {
+		t.Delay = TOTPDefaultDelay
 	}
 
 	if t.Algorithm == 0 {
@@ -64,4 +112,9 @@ func (t *TOTP) ensureKey() (err error) {
 	t.Key, err = randomKey(RandomKeyLength)
 
 	return err
+}
+
+// getCounter returns a valid counter based on the given timestamp.
+func (t *TOTP) getCounter(timestamp int64) uint64 {
+	return uint64(math.Floor(float64(timestamp) / float64(t.Period)))
 }
