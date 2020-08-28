7db778e8710e3e283333fdfdfa63fd241a73d745
Jose Torres <jose.torres.tm@gmail.com>
2020-08-28 16:41:35 -0500

    Update the HOTP example to explain more clearly the increments.

--- examples/hotp/main.go
+++ examples/hotp/main.go
@@ -19,11 +19,21 @@ func main() {
 		log.Panicf("unexpected error when generating OTP: %s", err)
 	}
 
+	h.Counter++ // Counter has to manually increased if you are not calling Validate()
 	otp36, err := h.Generate()
 	if err != nil {
 		log.Panicf("unexpected error when generating OTP: %s", err)
 	}
 
+	// Validate the last code
+	isValid1, err := h.Validate(otp36)
+	if err != nil {
+		log.Panicf("unexpected error when validating OTP: %s", err)
+	}
+
+	// This code will be the next one, even though we didn't call h.Counter++
+	// because successful validation will internally make the counter to go to
+	// the next expected counter.
 	otp37, err := h.Generate()
 	if err != nil {
 		log.Panicf("unexpected error when generating OTP: %s", err)
@@ -32,15 +42,6 @@ func main() {
 	msg := "Used key: %s\nGenerated codes:\n\t- %s\n\t- %s\n\t- %s\n"
 	fmt.Printf(msg, h.Key, otp35, otp36, otp37)
 
-	// Validate the last code
-	//     Since the token generation should be done in an external device, we
-	//     are rewinding the Counter one time.
-	h.Counter--
-	isValid1, err := h.Validate(otp37)
-	if err != nil {
-		log.Panicf("unexpected error when validating OTP: %s", err)
-	}
-
 	invalidToken := "123456"
 	isValid2, err := h.Validate(invalidToken)
 	if err != nil {
@@ -48,5 +49,5 @@ func main() {
 	}
 
 	msg = "Validated code:\n\t- %s --> %v\n\t- %s --> %v\n"
-	fmt.Printf(msg, otp37, isValid1, invalidToken, isValid2)
+	fmt.Printf(msg, otp36, isValid1, invalidToken, isValid2)
 }
